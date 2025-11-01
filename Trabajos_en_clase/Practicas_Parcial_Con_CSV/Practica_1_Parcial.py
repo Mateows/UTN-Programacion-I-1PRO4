d14b2683f0b317c4c345fc39b16ba150c0e9815d
Mateows <mateoliendo633@gmail.com>
2025-11-01 19:40:34 -0300

    erwwerwer

--- Trabajos_en_clase/Practicas_Parcial_Con_CSV/Practica_1_Parcial.py
+++ Trabajos_en_clase/Practicas_Parcial_Con_CSV/Practica_1_Parcial.py
@@ -3,101 +3,144 @@ import csv
 def inicializar_archivo():
     if not os.path.exists("inventario.csv"):
         with open("inventario.csv", "w", newline="", encoding="utf-8") as archivo:
-            encabezado = ["nombre", "stock"]
-            escritor = csv.DictWriter(archivo, fieldnames=encabezado)
-            escritor.writeheader()
+                escritor = csv.DictWriter(archivo, fieldnames=["nombre", "stock"])
+                escritor.writeheader()
 
-def cargar_datos():
+
+
+def cargar_archivo():
     try:
         lista = []
-        with open("inventario.csv", "r" ) as archivo:
+        with open("inventario.csv", "r", encoding="utf-8")as archivo:
             lector = csv.DictReader(archivo, fieldnames=["nombre", "stock"])
+            next(lector)
             for fila in lector:
                 try:
                     fila["stock"] = int(fila["stock"])
                     lista.append(fila)
                 except ValueError:
-                    print("Dato invalido")
+                    print(f"ERROR. Saltando fila{fila}")
         return lista
     except FileExistsError:
-        print("CSV Incorrecto")
-
-def actualizar_datos():
-    with open("inventario.csv", "w", encoding="utf-8", newline="") as archivo:
-        escritor = csv.DictWriter(archivo, fieldnames=["nombre","stock"])
-        escritor.writeheader
-        escritor.writerows()
-
-def mostrar_menu():
-    print("1- Cargar herramientas")
-    print("2- Mostrar herramientas registradas")
-    print("3- Modificar herramienta")
-    print("4- Eliminar herramienta")
-    print("5- Consultar disponibilidad")
-    print("6- Listar productos sin stock")
-    print("7- Salir")
+        print("ERROR. Archivo no encontrado")
 
 
 
+def actualizar_archivo(lista):
+    with open("inventario.csv", "w", newline="", encoding="utf-8") as archivo:
+            escritor = csv.DictWriter(archivo, fieldnames=["nombre", "stock"])
+            escritor.writeheader()
+            escritor.writerows(lista)
 
-def cargar_herramientas(inventario):
+
+def agregar_herramientas(lista):
     while True:
-        try:
-            herramientas_para_cargar = int(input("Ingrese cuantas herramientas queire cargar"))
-            for herramientas in range(herramientas_para_cargar):
-                nombre_herramienta = input(f"Nombre de la herramienta N°{herramientas+1}").title().strip()
-                if not nombre_herramienta.isalpha():
-                    print("ERROR. Intente con un caracter (a-z)")
+        cant_i = input("Ingrese la cantidad de herramientas a ingresar: ")
+        if not cant_i.isdigit():
+            print("Intente con un numero entero positivo")
+            continue
+        cant_i = int(cant_i)
+
+        for agregar in range(cant_i):
+            while True:
+                nombre_herr = input(f"Ingrese el nombre de la herramienta: {agregar+1}: ").strip().title()
+                if not nombre_herr.isalpha():
+                    print("Ingrese un nombre correcto")
                     continue
-                nombre_herramienta = str(nombre_herramienta)
+
+                nombre_herr = str(nombre_herr)
                 while True:
-                    
-        except ValueError:
-            print("Intente un número positivo")
+                    cant_stock = input(f"Ingrese la cantidad de stock para la herramienta {nombre_herr}: ")
+                    if not cant_stock.isdigit():
+                        print("Intente con un numero entero positivo")
+                        continue
+                    else:
+                        lista.append({"nombre": nombre_herr, "stock":cant_stock})
+                        break
+                break
+        actualizar_archivo(lista)
+        print("Datos actualizados")
+        return lista
+    
+
+
+def mostrar(lista):
+    for elem in lista:
+        print(f"{elem["nombre"]} - {elem["stock"]}")
+
+
+def modificar(lista):
+    while True:
+        buscar = input("Ingrese la herramienta a buscar:").title().strip()
+        if not buscar.isalpha():
+            print("Intente con una letra (a-z)")
+            continue
+        buscar = str(buscar)
+        for elem in lista:
+            if(buscar == elem["nombre"]):
+                print("Herramienta encontrada")
+                while True:
+                    try:
+                        agregar = int(input("Cuantas herramientas quiere agregar: "))
+                        elem["stock"] += agregar
+                        actualizar_archivo(lista)
+                        print("Archivo actualizado")
+                        return lista
+                    except ValueError:
+                        print("Numero invalido")
+
+def eliminar(lista):
+    lista_nueva = []
+    quitar = input("Que herramienta desea quitar: ").title().strip()
+    if not quitar.isalpha():
+        print("Valor invalido. Volviendo al menu")
+        return
+    else:
+        quitar = str(quitar)
+
+        for elem in lista:
+            if (quitar == elem["nombre"]):
+                continue
+            lista_nueva.append(elem)
+        actualizar_archivo(lista_nueva)
+        print("Elemento borrado correctamente")
+        return lista_nueva
+
+###CONTINUAR CON LA OPCION DE CONSULTAR DISPONIBILIDAD
+
+def menu_principal():
+    print("MENÚ")
+    print("1. Agregar herramientas")
+    print("2. Mostrar  herramientas")
+    print("3. Modificar herramienta")
+    print("4. Eliminar herramienta")
+    print("5. Consultar disponibilidad")
+    print("6. Listar productos sin stock")
+    print("7. Salir")
 
 
 
 def programa_principal():
     inicializar_archivo()
-    inventario = cargar_datos()
-    mostrar_menu()
+    inventario = cargar_archivo()
     while True:
         try:
-            opc = ("Ingrese una opcion")
+            menu_principal()
+            opc = int(input("Ingrese una opcion"))
+
             match opc:
                 case 1:
-                    inventario = cargar_herramientas(inventario)
+                    inventario = agregar_herramientas(inventario)
                 case 2:
-                    pass
+                    inventario = mostrar(inventario)
                 case 3:
-                    pass
+                    inventario = modificar(inventario)
                 case 4:
-                    pass
-                case 5:
-                    pass
-                case 6:
-                    pass
-                case 7:
-                    print("Adios")
-                    break
+                    inventario = eliminar(inventario)
                 case _:
-                    print("Valor invalido, intente de nuevo")
-
+                    print("Intente un numero valido")
         except ValueError:
-            print("Valor Invalido")
-
-
-
-
-
-
-
-
-
-
-
-
-
+            print("Intente con un valor válido")
 
 
 if __name__ == "__main__":
