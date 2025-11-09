e203967c7e54526029f1388abc91a41ff2d44b70
Mateows <mateoliendo633@gmail.com>
2025-11-09 14:43:02 -0300

    Practico Recursividad

---
+++ Unidad 9-Recursividad/Ejercicio1.py
@@ -0,0 +1,21 @@
+def factorial(usuario_num):
+    if n == 0:
+        reurn 1
+    else:
+        return n * factorial(n-1)
+
+
+
+
+while True:
+    try:
+        num = int(input("Ingrese un número: "))
+        except ValueError:
+            
+
+
+
+
+
+
+
